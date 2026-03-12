939176201e4a29e4d69568f310f00192719c26b6
Aurel <aurelgeorges@hotmail.fr>
2026-03-12 17:54:38 +0100

    Equipe

--- coin-coin-arena/src/Main.java
+++ coin-coin-arena/src/Main.java
@@ -1,11 +1,16 @@
 import canards.*;
+import equipe.Equipe;
 
 void main() {
     CanardDeCombat donald = new CanardFeu("Donald", "Donald", 100, 20, 0.8);
     CanardDeCombat daisy = new CanardEau("Daisy", "Daisy", 80, 25, 15);
 
+    Equipe equipe1 = new Equipe("Dresseur 1");
+    equipe1.ajouter(donald);
+    equipe1.ajouter(daisy);
+    equipe1.afficher();
 
-    donald.attaquer(daisy);
-    daisy.attaquer(donald);
+    equipe1.retirer("Donald");
+    equipe1.afficher();
 
 }
--- coin-coin-arena/src/equipe/Equipe.java
+++ coin-coin-arena/src/equipe/Equipe.java
@@ -1,4 +1,77 @@
 package equipe;
 
+import canards.CanardDeCombat;
+
+
 public class Equipe {
+    private CanardDeCombat[] canards = new CanardDeCombat[6];
+    private final String nomDresseur;
+
+    private static int nbEquipesCrees;
+
+    public Equipe(String nomDresseur) {
+        this.nomDresseur = nomDresseur;
+        nbEquipesCrees++;
+    }
+
+    public boolean ajouter(CanardDeCombat c) {
+        for (int i = 0; i < canards.length; i++) {
+            if (canards[i] == null) {
+                canards[i] = c;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void retirer(String surnom) {
+        for (int i = 0; i < canards.length; i++) {
+            if (canards[i] != null && canards[i].getSurnom().equals(surnom)) {
+                canards[i] = null;
+
+                for (int j = i; j < canards.length - 1; j++) {
+                    canards[j] = canards[j + 1];
+                }
+                canards[canards.length - 1] = null;
+            }
+        }
+    }
+
+    public CanardDeCombat getPremierValide() {
+        for (CanardDeCombat c : canards) {
+            if (c != null && !c.estKO()) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void soignerTous(){
+        for (CanardDeCombat c : canards) {
+            if (c != null) {
+                c.soigner();
+            }
+        }
+    }
+
+    public void afficher() {
+        System.out.println("Équipe de " + nomDresseur + " :");
+        for (int i = 0; i < canards.length; i++) {
+            if (canards[i] != null) {
+                System.out.println((i + 1) + ". " + canards[i]);
+            } else {
+                System.out.println((i + 1) + ". [Vide]");
+            }
+        }
+    }
+
+    public boolean touteKO(){
+        for (CanardDeCombat c : canards) {
+            if (c != null && !c.estKO()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
